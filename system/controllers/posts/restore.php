1bbd7ce553ee7073828a09d9eef29a45bd2848ff
nknwns <andrejshaev@gmail.com>
2025-10-28 19:25:13 +0700

    feat(back/posts): restore

---
+++ system/controllers/posts/restore.php
@@ -0,0 +1,27 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'] . '/global_pass.php');
+
+$availableTableIds = [33];
+
+$user = new Member($_COOKIE['member_id']);
+
+if ($user->is_valid()) {
+	$id      = (int)$_POST['id'];
+	$tableId = (int)$_POST['table_id'];
+
+	if (!in_array($tableId, $availableTableIds)) {
+		echo "You can't restore this post";
+		exit();
+	}
+
+	$table = (new Table($tableId))->tableName();
+	$post  = new Post($id);
+
+	$post->getTable($table);
+
+	$post->postRestore();
+
+	header("Location: " . $_SERVER['HTTP_REFERER']);
+} else {
+	echo "F*ck you, hacker=)";
+}
